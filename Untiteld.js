aaa810582094e3d9dafd0afc66c6388cc64a6cb0
firas hajji <firashajji59@gmail.co>
2023-06-02 11:10:48 +0100

    changes

--- Untiteld.js
+++ Untiteld.js
@@ -1,14 +1,16 @@
-let D1=document.getElementById("D1");
-const  start=()=>{
-let date=new Date();
-let hh=date.getHours();
-let mm=date.getMinutes();
-let ss=date.getSeconds();
-hh=hh==0? hh="12":hh;
-hh=hh<10? hh="0"+hh:hh;
-mm=mm<10? mm="0"+mm:mm;
-ss=ss<10?  ss="0"+ss:ss;
-D1.textContent=hh+":"+mm+":"+ss;
+let D = document.getElementById("D1");
+const start = () => {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    hh = hh == 0 ? hh = "12" : hh;
+    hh = hh < 10 ? hh = "0" + hh : hh;
+    mm = mm < 10 ? mm = "0" + mm : mm;
+    ss = ss < 10 ? ss = "0" + ss : ss;
+    D.textContent = hh + ":" + mm + ":" + ss;
+
 }
+//function 
 start();
-setInterval(start,1000);
+setInterval(start, 1000);
